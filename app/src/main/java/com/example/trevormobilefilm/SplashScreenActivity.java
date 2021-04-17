97c9a21ea3804fe41028fec4da0c3065af7a5e2c
TheDusKnight <860956020@qq.com>
2021-04-17 08:42:18 -0700

    Splash basic layout finish

--- app/src/main/java/com/example/trevormobilefilm/SplashScreenActivity.java
+++ app/src/main/java/com/example/trevormobilefilm/SplashScreenActivity.java
@@ -2,14 +2,15 @@ package com.example.trevormobilefilm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        setTheme(R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
     }
 }
