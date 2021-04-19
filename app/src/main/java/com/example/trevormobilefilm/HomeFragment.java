2bbc24e54e6a5328ea81d6efd064a4eeff259eb1
TheDusKnight <860956020@qq.com>
2021-04-18 22:22:29 -0700

    Create Volley sample textView in activity_main.xml, begin Search fragment

--- app/src/main/java/com/example/trevormobilefilm/HomeFragment.java
+++ app/src/main/java/com/example/trevormobilefilm/HomeFragment.java
@@ -1,18 +1,32 @@
 package com.example.trevormobilefilm;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+//        final TextView textView = (TextView) view.findViewById(R.id.home_text);
+//
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//
+//
+//        Log.d("CREATION", String.valueOf(textView));
+
+        return view;
     }
 }
--- app/src/main/java/com/example/trevormobilefilm/MainActivity.java
+++ app/src/main/java/com/example/trevormobilefilm/MainActivity.java
@@ -6,7 +6,14 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
 //        setTheme(R.style.SplashScreenTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://10.0.2.2:8080/top/tv";
+//        String url = "https://www.google.com";
+//        String url = "https://trevor-imdb-framework.wm.r.appspot.com/pop/movie";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    // Display the first 500 characters of the response string.
+                    textView.setText("Response is: "+ response.substring(0,500));
+                }, error -> textView.setText(error.toString()));
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
