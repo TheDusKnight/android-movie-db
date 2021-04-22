3bb36e6b66072eacbcc4264eb34cdcf4d480ccb0
TheDusKnight <860956020@qq.com>
2021-04-22 04:20:50 -0700

    detail page basic info and youtube img replace finish

--- app/src/main/java/com/example/trevormobilefilm/DetailActivity.java
+++ app/src/main/java/com/example/trevormobilefilm/DetailActivity.java
@@ -8,11 +8,13 @@ import androidx.lifecycle.ViewModelProvider;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.bumptech.glide.Glide;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
@@ -21,9 +23,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class DetailActivity extends AppCompatActivity {
     private WatchViewModel detailViewModel;
     RequestQueue mQueue;
+    TextView titleView;
+    TextView overView;
+    TextView genresView;
+    TextView yearView;
+    ImageView backDropView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,38 +54,75 @@ public class DetailActivity extends AppCompatActivity {
 
         // Send request
         mQueue = VolleySingleton.getInstance(this).getRequestQueue();
-        jsonParse(MainActivity.URL + "/" + filmType + "/video/" + filmId, video -> {
-            String videoId = "";
+        jsonParse(MainActivity.URL + "/" + filmType + "/detail/" + filmId, detail -> {
+            String title = "N/A";
+            String overview = "N/A";
+            String genres = "N/A";
+            String year = "N/A";
+            String backDropImage = null;
             try {
-                videoId = video.getJSONObject(0).getString("key");
+                title = detail.getJSONObject(0).getString("name")
+                        != null ? detail.getJSONObject(0).getString("name") : "N/A";
+                overview = detail.getJSONObject(0).getString("overview")
+                        != null ? detail.getJSONObject(0).getString("overview") : "N/A";
+                List<String> genresList = new ArrayList<>();
+                JSONArray genresJson = detail.getJSONObject(0).getJSONArray("genres");
+                if (genresJson != null && genresJson.length() > 0) {
+                    for (int i = 0; i < genresJson.length(); i++) {
+                        genresList.add(genresJson.getString(i));
+                    }
+                    genres = String.join(", ", genresList);
+                }
+                String dateString = detail.getJSONObject(0).getString("release_date");
+                if (dateString != null) {
+                    List<String> dataList = Arrays.asList(dateString.split("-"));
+                    year = dataList.get(0);
+                }
+                backDropImage = detail.getJSONObject(0).getString("backdrop_path");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            // Create YouTube player or set background image
-            createYouTube(videoId);
+            titleView = findViewById(R.id.title_text);
+            titleView.setText(title);
+            overView = findViewById(R.id.overview_text);
+            overView.setText(overview);
+            genresView = findViewById(R.id.genres_text);
+            genresView.setText(genres);
+            yearView = findViewById(R.id.year_text);
+            yearView.setText(year);
+
+            String finalBackDropImage = backDropImage;
+            jsonParse(MainActivity.URL + "/" + filmType + "/video/" + filmId, video -> {
+                String videoId = "";
+                try {
+                    videoId = video.getJSONObject(0).getString("key");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                // Create YouTube player or set background image
+                createYouTube(videoId, finalBackDropImage);
+            });
         });
-
-        TextView textView = findViewById(R.id.detailText);
-        textView.setText(filmId + filmType);
-
 
     }
 
-    private void createYouTube(String videoId) {
+    private void createYouTube(String videoId, String backDropImage) {
         // Add YouTube Player as lifecycle observer
         YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
         if (!videoId.equals("tzkWB85ULJY") && !videoId.isEmpty()) {
             getLifecycle().addObserver(youTubePlayerView);
 
             youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-                                                           @Override
-                                                           public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                @Override
+                public void onReady(@NonNull YouTubePlayer youTubePlayer) {
                     youTubePlayer.cueVideo(videoId, 0);
-            }
+                }
             });
         } else {
-            // TODO: 如果video key是tzkWB85ULJY，那么隐藏YouTube，显示backdrop_path图片
-            youTubePlayerView.setVisibility(View.INVISIBLE);
+            youTubePlayerView.setVisibility(View.GONE);
+            backDropView = findViewById(R.id.back_drop_view);
+            Glide.with(this).load(backDropImage).into(backDropView);
+            backDropView.setVisibility(View.VISIBLE);
         }
     }
 
