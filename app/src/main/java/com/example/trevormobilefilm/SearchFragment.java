3d2efe390041c0e4286983d25d270f8c5c6c5e58
TheDusKnight <860956020@qq.com>
2021-04-24 14:57:05 -0700

    search fragment fix empty textView bug

--- app/src/main/java/com/example/trevormobilefilm/SearchFragment.java
+++ app/src/main/java/com/example/trevormobilefilm/SearchFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -34,10 +35,12 @@ public class SearchFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         SearchView searchView = view.findViewById(R.id.search_view);
+        TextView noView = view.findViewById(R.id.search_no_view);
 
         mQueue = VolleySingleton.getInstance(this.getContext()).getRequestQueue();
 
         searchView.setIconified(false);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -47,34 +50,37 @@ public class SearchFragment extends Fragment {
             @Override
             public boolean onQueryTextChange(String newText) {
                 try {
-                    jsonParse(MainActivity.URL + "/search/multi/" + URLEncoder.encode(newText,
-                            StandardCharsets.UTF_8.toString()), searchFilms -> {
-                        ArrayList<CardData> cardSearchDataArrayList = new ArrayList<>();
-                        RecyclerView searchScrollView = view.findViewById(R.id.search_scroller);
-                        TextView noView = view.findViewById(R.id.search_no_view);
+                    if (newText.length() == 0) {
+                        noView.setVisibility(View.INVISIBLE);
+                    } else {
+                        jsonParse(MainActivity.URL + "/search/multi/" + URLEncoder.encode(newText,
+                                StandardCharsets.UTF_8.toString()), searchFilms -> {
+                            ArrayList<CardData> cardSearchDataArrayList = new ArrayList<>();
+                            RecyclerView searchScrollView = view.findViewById(R.id.search_scroller);
 
-                        if (searchFilms.length() < 1) {
-                            noView.setVisibility(View.VISIBLE);
-                        } else {
-                            noView.setVisibility(View.INVISIBLE);
-                        }
+                            if (searchFilms.length() < 1) {
+                                noView.setVisibility(View.VISIBLE);
+                            } else {
+                                noView.setVisibility(View.INVISIBLE);
+                            }
 
-                        for (int i = 0; i < searchFilms.length(); i++) {
-                            JSONObject mObject = searchFilms.getJSONObject(i);
-                            String backPath = mObject.getString("backdrop_path");
-                            String filmType = mObject.getString("media_type");
-                            String filmName = mObject.getString("name");
-                            int filmId = mObject.getInt("id");
-                            int filmRate = mObject.getInt("rate");
-                            String filmDate = mObject.getString("date");
-                            cardSearchDataArrayList.add(new CardData(backPath, filmType, filmName, filmId, filmDate, filmRate));
-                        }
+                            for (int i = 0; i < searchFilms.length(); i++) {
+                                JSONObject mObject = searchFilms.getJSONObject(i);
+                                String backPath = mObject.getString("backdrop_path");
+                                String filmType = mObject.getString("media_type");
+                                String filmName = mObject.getString("name");
+                                int filmId = mObject.getInt("id");
+                                int filmRate = mObject.getInt("rate");
+                                String filmDate = mObject.getString("date");
+                                cardSearchDataArrayList.add(new CardData(backPath, filmType, filmName, filmId, filmDate, filmRate));
+                            }
 
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-                        SearchAdapter searchAdapter = new SearchAdapter(getContext(), cardSearchDataArrayList);
-                        searchScrollView.setLayoutManager(layoutManager);
-                        searchScrollView.setAdapter(searchAdapter);
-                    });
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                            SearchAdapter searchAdapter = new SearchAdapter(getContext(), cardSearchDataArrayList);
+                            searchScrollView.setLayoutManager(layoutManager);
+                            searchScrollView.setAdapter(searchAdapter);
+                        });
+                    }
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
